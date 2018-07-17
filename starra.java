258d86b88e22884b886aaacea00de5b2cada7e30
Reenu-mohan <41322061+Reenu-mohan@users.noreply.github.com>
2018-07-17 17:03:25 +0530

    Add files via upload

---
+++ starra.java
@@ -0,0 +1,27 @@
+
+public class starra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String arr[]= {"reenu","jilu","guru","athira","gopika"};
+		int f=0,i;
+		for( i=0;i<(arr.length);i++)
+		{
+			if(arr[i]=="guru")
+			{
+				f=1;
+				
+				break;
+			}}
+		
+		
+		if(f==1) 
+			System.out.println("guru is found at "+(i+1)+" position");
+			
+		else
+			
+			System.out.println("guru is not found ");
+		
+	}
+
+}
