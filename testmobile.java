adc73c5388542f8f6a36916065c4f0836db9d2c7
Reenu-mohan <41322061+Reenu-mohan@users.noreply.github.com>
2018-07-17 17:19:18 +0530

    Add files via upload

---
+++ testmobile.java
@@ -0,0 +1,16 @@
+
+public class testmobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		mobile m1= new mobile();
+		m1.price=15000;
+		m1.brandname="samsung";
+		double kk= m1.discount();
+		System.out.println("discount "+kk);
+		
+		
+	}
+
+}
