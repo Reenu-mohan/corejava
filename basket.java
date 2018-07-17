cf0f8f1439ede2428b9f2b3a01d51ac9d7a887fb
Reenu-mohan <41322061+Reenu-mohan@users.noreply.github.com>
2018-07-17 17:04:43 +0530

    Add files via upload

---
+++ basket.java
@@ -0,0 +1,25 @@
+
+public class basket {
+
+	public static void main(String[] args) {
+		
+		String bas1[]= {"red","red","green","orange"};
+		String bas2[]= {"red","red","green","orange"};
+		int f=0;
+		for(int i=0;i<bas1.length;i++)
+		{
+			if(bas1[i]=="red")
+			{
+				f++;
+			}
+		}
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
