c0a8fd931d79dfa778f77e4cd93e847e66c80d42
Reenu-mohan <41322061+Reenu-mohan@users.noreply.github.com>
2018-07-17 17:09:45 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,19 @@
+
+public class largest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=0,n2=0;
+		if(n1>n2)
+			System.out.println("largest no is "+n1);
+		else
+			{if(n2>n1)
+			System.out.println("largest no is "+n2);
+			else 
+				if(n1==0 && n2==0)
+					System.out.println("both cant be zero");
+		else
+			System.out.println("both are equal");
+	}}
+
+}
