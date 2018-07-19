78e682356a66954fe73560f028752635716ee7f9
Reenu-mohan <41322061+Reenu-mohan@users.noreply.github.com>
2018-07-19 13:39:26 +0530

    Add files via upload

---
+++ numpatt.java
@@ -0,0 +1,18 @@
+
+public class numpatt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d=0;
+		for (int i=0;i<=6;i++)
+			
+		{
+			
+			for (int j=0;j<=5;j++)
+				{System.out.print(j+d);		
+				d=d+i;	
+				}	
+	}
+
+}
+}
